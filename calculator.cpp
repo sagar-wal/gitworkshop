8be6dce75e2a0e97660623e2a7c8e84bb6a4c9bb
sagar-wal <53569839+sagar-wal@users.noreply.github.com>
2019-10-01 02:19:53 +0530

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -6,13 +6,13 @@ void add(int a, int b)    // function to add two numbers
   cout<<"Sum of 2 numbers "<<a<<" and "<<b<<" is "<<c;
 }
 
-void sub(int a, int b)
+void sub(int a, int b)   // function to subtract two numbers 
 {
   int c=a-b;
   cout<<"Difference of 2 numbers "<<a<<" and "<<b<<" is "<<c;
 }
 
-void mul(int a, int b)    // function to add two numbers
+void mul(int a, int b)    // function to multiply two numbers
 {
   int c=a*b;
   cout<<"Product of 2 numbers "<<a<<" and "<<b<<" is "<<c;

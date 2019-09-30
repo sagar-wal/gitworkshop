62a4f2bcd1559ca2983ca73f956c04d009c40de9
sagar-wal <sagaragarwal612@gmail.com>
2019-10-01 02:18:21 +0530

    product func

--- calculator.cpp
+++ calculator.cpp
@@ -12,3 +12,8 @@ void sub(int a, int b)
   cout<<"Difference of 2 numbers "<<a<<" and "<<b<<" is "<<c;
 }
 
+void mul(int a, int b)    // function to add two numbers
+{
+  int c=a*b;
+  cout<<"Product of 2 numbers "<<a<<" and "<<b<<" is "<<c;
+}

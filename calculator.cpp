4631d78d7be9ae32347233384ecec577f17d2533
sagar-wal <53569839+sagar-wal@users.noreply.github.com>
2019-10-01 09:24:55 +0530

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -17,3 +17,9 @@ void mul(int a, int b)    // function to add two numbers
   int c=a*b;
   cout<<"Product of 2 numbers "<<a<<" and "<<b<<" is "<<c;
 }
+
+void div(int a, int b)
+{
+  int c=a/b;
+  cout<<"Quotient when "<<a<<" is divided by "<<b<<" is "<<c;
+}
